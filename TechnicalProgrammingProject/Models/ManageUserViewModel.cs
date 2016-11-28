9195a901d7155dbdc3d3fc36cce7ac3508ec771a
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2016-11-28 09:07:10 -0800

    Uploaded user displays on Recipe Page. Added very rough UserViewModel
    Signed-off-by: Connor Phalen <Phalen.Connor@hotmail.co.uk>

---
+++ TechnicalProgrammingProject/Models/ManageUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechnicalProgrammingProject.Models
+{
+    public class ManageUserViewModel
+    {
+        public int UserID { get; set; }
+        public string UserDisplayName { get; set; }
+        public string UserRole { get; set; }
+        // public DateTime DateJoined { get; set; }
+        // public int NumOfRecipes { get; set; }
+
+        public string newStatus { get; set; }
+    }
+}
