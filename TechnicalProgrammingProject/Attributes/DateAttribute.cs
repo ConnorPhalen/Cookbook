2bb217e83390e596c21b66ef763d43924562c059
Xavier Lau <xavierlau1@gmail.com>
2016-12-04 15:18:14 -0800

    Add date validaiton attribute.

---
+++ TechnicalProgrammingProject/Attributes/DateAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TechnicalProgrammingProject.Attributes
+{
+    public class DateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime date = (DateTime)value;
+            DateTime startDate = Convert.ToDateTime("01/01/1900");
+            DateTime endDate = Convert.ToDateTime("01/01/2100");
+
+            if (date > endDate || date < startDate)
+            {
+                return new ValidationResult(string.Format("Date must be between {0} and {1}.", startDate, endDate));
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
