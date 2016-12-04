8f4ee9754de5b868365731be72dc15aaccd13749
Xavier Lau <xavierlau1@gmail.com>
2016-12-03 23:03:07 -0800

    Add custom validation attribute for image uploads.

--- TechnicalProgrammingProject/Models/RecipeViewModels.cs
+++ TechnicalProgrammingProject/Models/RecipeViewModels.cs
@@ -18,6 +18,7 @@ namespace TechnicalProgrammingProject.Models
         [StringLength(50)]
         public string Name { get; set; }
         [Required]
+        [StringLength(500)]
         public string Description { get; set; }
 
         [Required]
@@ -28,12 +29,86 @@ namespace TechnicalProgrammingProject.Models
         [Range(0,100)]
         public int Servings { get; set; }
 
-        [HttpPostedFileExtensions]
-        [ValidateFileSize(ErrorMessage = "Image must be less than 1MB")]
+        [Image]
         public HttpPostedFileBase Image { get; set; }
         [Required]
         public string Directions { get; set; }
         public virtual ICollection<Ingredient> Ingredients { get; set; }
+    }
+
+    /// <summary>
+    /// Model used to display information available for editting.
+    /// </summary>
+    public class EditRecipeViewModel
+    {
+        /// <summary>
+        /// Initialize the Ingredients for the recipe.
+        /// </summary>
+        public EditRecipeViewModel()
+        {
+            Ingredients = new HashSet<Ingredient>();
+        }
+
+        /// <summary>
+        /// ID of the recipe to edit.
+        /// </summary>
+        [Required]
+        public int ID { get; set; }
+        /// <summary>
+        /// Name of the recipe.
+        /// </summary>
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of the recipe.
+        /// </summary>
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Time needed to cook the recipe.
+        /// </summary>
+        [Required]
+        [DisplayName("Cook Time")]
+        [Range(0, 2880)]
+        public int CookTime { get; set; }
+
+        /// <summary>
+        /// Number of servings the recipe provides.
+        /// </summary>
+        [Required]
+        [Range(0, 100)]
+        public int Servings { get; set; }
+
+        /// <summary>
+        /// Image upload of the recipe.
+        /// </summary>
+        [Image]
+        public HttpPostedFileBase RecipePicture { get; set; }
+
+        /// <summary>
+        /// Actual Image of the Recipe.
+        /// </summary>
+        public byte[] Image { get; set; }
+
+        /// <summary>
+        /// Directions to cook the recipe.
+        /// </summary>
+        [Required]
+        public string Directions { get; set; }
+
+        /// <summary>
+        /// Ingredients required to make the recipe.
+        /// </summary>
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+
+        /// <summary>
+        /// Tags used by searching.
+        /// </summary>
+        public virtual ICollection<Tag> Tags { get; set; }
     }
 
     public class UploadsViewModel
