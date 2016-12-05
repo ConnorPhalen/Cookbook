95b05dbdea4a4e95c2831920c2c9af833ab9356e
Lei Zhang(Daniel) <danielvmaple@gmail.com>
2016-12-04 18:01:36 -0800

    Finished my tasks
    Preview image when uploading a recipe. (DONE!)
    Preview image when uploading a profile. (DONE!)
    Style Edit Profile View. (DONE!)
    Add back button in Edit Profile View. (DONE!)

---
+++ TechnicalProgrammingProject/Content/asset/js/preview.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('#ProfilePicture').on("change", function () {
+        var file = this.files[0];
+
+        if (this.files && file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#Avatar').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(file);
+        }
+    });
+
+    $('#backBtn').click(function (ev) {
+        ev.preventDefault();
+        history.back();
+    });
+});
+
