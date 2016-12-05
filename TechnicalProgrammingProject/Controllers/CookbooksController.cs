b8948b3c0910bbb2fa9383d952d1a87232558c69
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2016-12-04 20:58:27 -0800

    Added rough code to add and remove recipes from cookbook. Doesnt fully work, and the visuals are lame,  ut will fix it later.
    Signed-off-by: Connor Phalen <Phalen.Connor@hotmail.co.uk>

--- TechnicalProgrammingProject/Controllers/CookbooksController.cs
+++ TechnicalProgrammingProject/Controllers/CookbooksController.cs
@@ -152,6 +152,56 @@ namespace TechnicalProgrammingProject.Controllers
             }
         }
 
+        /// <summary>
+        /// Checks to see of the user has this recipe in their Cookbook.
+        /// If true, disply a 'Remove From Cookbook' button.
+        /// If false, display an 'Add to Cookbook' button.
+        /// </summary>
+        /// <param name="recID"></param>
+        /// <returns></returns>
+        public HtmlString cookbookCheck(int? recID)
+        {
+            var userID = User.Identity.GetUserId();
+
+            var isInCookbook = db.Recipes.Where(r => r.ID == recID && r.Cookbooks.Any(c => c.ApplicationUser.Id == userID));
+
+            if (isInCookbook.Count() == 0)
+            {
+                // return true;
+                return new HtmlString("<input class=\"btn btn - primary hvr - grow - shadow\" type=\"submit\" value=\"add\" name=\"option\" />");
+            }
+            else
+            {
+                //return false;
+                return new HtmlString("<input class=\"btn btn - primary hvr - grow - shadow\" type=\"submit\" value=\"remove\" name=\"option\" />");
+            }
+        }
+
+        /// <summary>
+        /// Adds or Removes the Recipe to the current users Cookbook.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult modifyCookbookRecipe(string option, int recID)
+        {
+            string userID = User.Identity.GetUserId();
+
+            Recipe result = db.Recipes.Where(r => r.ID == recID).Select(r => r).Single();
+
+            Cookbook userCookbook = db.Cookbooks.Where(c => c.ApplicationUserID == userID).Select(c => c).Single();
+            
+            // if the user wants to add the recipe to their cookbook...
+            if (option.Equals("add"))
+            {
+                userCookbook.Recipes.Add(result);
+            }
+            else
+            {
+                userCookbook.Recipes.Remove(result);
+            }
+
+            return RedirectToAction("Details", "Recipes", new { id = recID });
+        }
 
         protected override void Dispose(bool disposing)
         {
